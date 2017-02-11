836c39f0ce01137e66875870e83d0a129d145125
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-02-11 15:27:48 +0100

    Linted whole codebase to standard

--- test/buffer.test.js
+++ test/buffer.test.js
@@ -1,122 +1,121 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var vfs = require('vinyl-fs');
-var test = require('tape');
-var readdirp = require('readdirp');
-var rimraf = require('rimraf');
-var converter = require('../');
+var fs = require('fs')
+var vfs = require('vinyl-fs')
+var test = require('tape')
+var readdirp = require('readdirp')
+var rimraf = require('rimraf')
+var converter = require('../')
 
-var exist = fs.existsSync;
+var exist = fs.existsSync
 var read = function (path) {
-  return fs.readFileSync(path, 'utf8');
-};
+  return fs.readFileSync(path, 'utf8')
+}
 
 test('before', function (assert) {
-  assert.plan(1);
+  assert.plan(1)
 
   vfs.src('test/fixture/input/**/*.+(sass|scss|css)')
     .pipe(converter({
       from: 'sass',
-      to: 'scss',
+      to: 'scss'
     }))
     .on('error', function (err) {
-      assert.fail(err.message);
-      assert.end();
+      assert.fail(err.message)
+      assert.end()
     })
     .pipe(vfs.dest('test/output'))
     .on('end', function () {
-      assert.pass('Converter successfully run');
-      assert.end();
-    });
-
-});
+      assert.pass('Converter successfully run')
+      assert.end()
+    })
+})
 
 test('output#structure', function (assert) {
-  assert.plan(5);
+  assert.plan(5)
 
   assert.ok(
     exist('test/output'),
     'Should create an `output` dir'
-  );
+  )
   assert.ok(
     exist('test/output/css'),
     'Should create a `css` dir'
-  );
+  )
   assert.ok(
     exist('test/output/sass'),
     'Should create a `sass` dir'
-  );
+  )
   assert.ok(
     exist('test/output/scss'),
     'Should create a `scss` dir'
-  );
+  )
 
-  var result = [];
+  var result = []
   var expected = [
     'css/one.css',
     'css/two.css',
     'sass/one.sass',
     'sass/two.sass',
     'scss/one.scss',
-    'scss/two.scss',
-  ];
+    'scss/two.scss'
+  ]
 
   readdirp({ root: 'test/output', fileFilter: '**/*.+(sass|scss|css)' })
     .on('data', function (entry) {
-      result.push(entry.path);
+      result.push(entry.path)
     })
     .on('end', function () {
-      assert.deepEqual(result.sort(), expected.sort(), 'All chunks should pass through');
-      assert.end();
-    });
-});
+      assert.deepEqual(result.sort(), expected.sort(), 'All chunks should pass through')
+      assert.end()
+    })
+})
 
 test('output#sass', function (assert) {
-  assert.plan(1);
+  assert.plan(1)
 
-  var result = read('test/output/sass/one.sass');
-  var expected = read('test/fixture/expected/sass/one.sass');
+  var result = read('test/output/sass/one.sass')
+  var expected = read('test/fixture/expected/sass/one.sass')
 
   assert.equal(
     result,
     expected,
     'Should properly convert Sass to SCSS'
-  );
+  )
 
-  assert.end();
-});
+  assert.end()
+})
 
 test('output#scss', function (assert) {
-  assert.plan(1);
+  assert.plan(1)
 
-  var result = read('test/output/scss/one.scss');
-  var expected = read('test/fixture/input/scss/one.scss');
+  var result = read('test/output/scss/one.scss')
+  var expected = read('test/fixture/input/scss/one.scss')
 
   assert.equal(
     result,
     expected,
     'Should not modify SCSS files'
-  );
+  )
 
-  assert.end();
-});
+  assert.end()
+})
 
 test('output#css', function (assert) {
-  assert.plan(1);
+  assert.plan(1)
 
-  var result = read('test/output/css/one.css');
-  var expected = read('test/fixture/input/css/one.css');
+  var result = read('test/output/css/one.css')
+  var expected = read('test/fixture/input/css/one.css')
 
   assert.equal(
     result,
     expected,
     'Should not modify CSS files'
-  );
+  )
 
-  assert.end();
-});
+  assert.end()
+})
 
 test('after', function (assert) {
-  rimraf('test/output', assert.end);
-});
+  rimraf('test/output', assert.end)
+})
--- test/rename.test.js
+++ test/rename.test.js
@@ -1,68 +1,66 @@
-'use strict';
+'use strict'
 
-var path = require('path');
-var vfs = require('vinyl-fs');
-var through = require('through2');
-var test = require('tape');
-var converter = require('../');
+var path = require('path')
+var vfs = require('vinyl-fs')
+var through = require('through2')
+var test = require('tape')
+var converter = require('../')
 
 test('rename#true', function (assert) {
-  assert.plan(1);
+  assert.plan(1)
 
   vfs.src('test/fixture/input/sass/one.sass')
     .pipe(converter({
       from: 'sass',
       to: 'scss',
-      rename: true,
+      rename: true
     }))
     .on('error', function (err) {
-      assert.fail(err.message);
-      assert.end();
+      assert.fail(err.message)
+      assert.end()
     })
     .pipe(through.obj(function (file, _, cb) {
-      var ext = path.extname(file.path);
+      var ext = path.extname(file.path)
 
       assert.equal(
         ext,
         '.scss',
         'Should properly rename .sass file to .scss'
-      );
+      )
 
-      cb();
+      cb()
     }))
     .on('end', function () {
-      assert.pass('Converter successfully run');
-      assert.end();
-    });
-
-});
+      assert.pass('Converter successfully run')
+      assert.end()
+    })
+})
 
 test('rename#false', function (assert) {
-  assert.plan(1);
+  assert.plan(1)
 
   vfs.src('test/fixture/input/scss/one.scss')
     .pipe(converter({
       from: 'scss',
-      to: 'sass',
+      to: 'sass'
     }))
     .on('error', function (err) {
-      assert.fail(err.message);
-      assert.end();
+      assert.fail(err.message)
+      assert.end()
     })
     .pipe(through.obj(function (file, _, cb) {
-      var ext = path.extname(file.path);
+      var ext = path.extname(file.path)
 
       assert.equal(
         ext,
         '.scss',
         'Should not rename .scss file'
-      );
+      )
 
-      cb();
+      cb()
     }))
     .on('end', function () {
-      assert.pass('Converter successfully run');
-      assert.end();
-    });
-
-});
+      assert.pass('Converter successfully run')
+      assert.end()
+    })
+})
